e8dbda20948f914d5f58d056f90555c47f1fd2b4
Meltem Şenol <meltem.altindal@gmail.com>
2025-07-25 22:36:29 +0300

    Wheather classes remove

---
+++ Ymyp67CvProject.Business/Concrete/InterestManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ymyp67CvProject.Business.Concrete
+{
+    internal class InterestManager
+    {
+    }
+}
