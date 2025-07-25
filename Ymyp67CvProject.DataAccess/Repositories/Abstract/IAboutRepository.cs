68105c9c9c928e0f28a2144b4140195913d6ab8a
Meltem Şenol <meltem.altindal@gmail.com>
2025-07-25 20:09:31 +0300

    EntityRepository interface created.

---
+++ Ymyp67CvProject.DataAccess/Repositories/Abstract/IAboutRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ymyp67CvProject.Entity.Concrete;
+
+namespace Ymyp67CvProject.DataAccess.Repositories.Abstract
+{
+    public interface IAboutRepository:IGenericRepository<About>
+    {
+    }
+}
