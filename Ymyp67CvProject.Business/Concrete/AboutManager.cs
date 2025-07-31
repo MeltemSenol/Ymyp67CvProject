07ef8cca685c343881726efc618781355afd6651
Meltem Şenol <meltem.altindal@gmail.com>
2025-07-31 20:38:33 +0300

    About manager completed

--- Ymyp67CvProject.Business/Concrete/AboutManager.cs
+++ Ymyp67CvProject.Business/Concrete/AboutManager.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Core.UnitOfWorks;
 using Core.Utilities.Results;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Ymyp67CvProject.Business.Abstract;
 using Ymyp67CvProject.DataAccess.Abstract;
+using Ymyp67CvProject.Entity.Concrete;
 using Ymyp67CvProject.Entity.Dtos.About;
 
 namespace Ymyp67CvProject.Business.Concrete
@@ -25,29 +27,99 @@ namespace Ymyp67CvProject.Business.Concrete
             _unitOfWork = unitOfWork;
         }
 
-        public Task<IDataResult<AboutResponseDto>> AddAsync(AboutCreateRequestDto dto)
+        public async Task<IDataResult<AboutResponseDto>> AddAsync(AboutCreateRequestDto dto)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var about = _mapper.Map<About>(dto);
+                await _aboutRepository.AddAsync(about);
+
+                await _unitOfWork.CommitAsync();
+
+                var response=_mapper.Map<AboutResponseDto>(about);
+               return new SuccessDataResult<AboutResponseDto>(response, "About information added successfully.");
+            }
+            catch (Exception e)
+            {
+                return new ErrorDataResult<AboutResponseDto>(e.Message);
+            }
+        }
+        public async Task<IResult> UpdateAsync(AboutUpdateRequestDto dto)
+        {
+            try
+            {
+                var about = _mapper.Map<About>(dto);  //update ve remove için async yok. Fakat belki başka metodlar yazarız buraya diye public async olarak tanımladık.
+                about.UpdateAt = DateTime.Now;
+                _aboutRepository.Update(about); 
+                await _unitOfWork.CommitAsync();
+                return new SuccessResult("About updated successfully.");
+            }
+            catch (Exception e)
+            {
+
+                return new ErrorResult(e.Message);
+            }
+        }
+        public async Task<IResult> RemoveAsync(Guid id)
+        {
+            try
+            {
+                var about = await _aboutRepository.GetAsync(a=>a.Id==id);
+                if(about is null)
+                {
+                    return new ErrorResult("About not found.");
+                }
+                about.UpdateAt = DateTime.Now;
+                about.IsDeleted = true; 
+                about.IsActive = false; 
+                _aboutRepository.Update(about);
+                await _unitOfWork.CommitAsync();
+                return new SuccessResult("About removed successfully.");
+            }
+            catch (Exception e)
+            {
+                return new ErrorResult(e.Message);
+            }
+        }
+        public async Task<IDataResult<AboutResponseDto>> GetByIdAsync(Guid id)
+        {
+            try
+            {
+                var about = await _aboutRepository.GetAsync(a => a.Id == id);
+                if (about is null)
+                {
+                    return new ErrorDataResult<AboutResponseDto>("About not found.");
+                }
+                var response = _mapper.Map<AboutResponseDto>(about);
+                return new SuccessDataResult<AboutResponseDto>(response,"About get successfully.");
+            }
+            catch (Exception e)
+            {
+
+                return new ErrorDataResult<AboutResponseDto>(e.Message);
+            }
+        }
+        public async Task<IDataResult<IEnumerable<AboutResponseDto>>> GetAllAsync()
+        {
+            try
+            {
+                var abouts=await _aboutRepository.GetAll(a=>!a.IsDeleted).ToListAsync();
+                if (abouts is null)
+                {
+                    return new ErrorDataResult<IEnumerable<AboutResponseDto>>("About list not found.");
+                }
+                var dtos= _mapper.Map<IEnumerable<AboutResponseDto>>(abouts);
+                return new SuccessDataResult<IEnumerable<AboutResponseDto>>(dtos,"About list get successfully.");
+            }
+            catch (Exception e)
+            {
+                return new ErrorDataResult<IEnumerable<AboutResponseDto>>(e.Message);
+            }
         }
 
-        public Task<IDataResult<IEnumerable<AboutResponseDto>>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
+        
 
-        public Task<IDataResult<AboutResponseDto>> GetByIdAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IResult> RemoveAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IResult> UpdateAsync(AboutUpdateRequestDto dto)
-        {
-            throw new NotImplementedException();
-        }
+      
+       
     }
 }
